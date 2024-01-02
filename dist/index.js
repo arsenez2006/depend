256ff045b66055e2731678f0e221e20ca3599cc3
arsenez <arsenez@cybercommunity.space>
2024-01-02 18:47:04 +0300

    Download installer

--- dist/index.js
+++ dist/index.js
@@ -82896,27 +82896,78 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-const path = __nccwpck_require__(1017);
 const action = __nccwpck_require__(2810);
 const cache = __nccwpck_require__(9535);
-const io = __nccwpck_require__(9913)
+const io = __nccwpck_require__(9913);
 const { request } = __nccwpck_require__(4923);
 
-async function download_installer() {
-    const release = await request("GET /repos/{owner}/{repo}/releases/latest", {
-        owner: "arsenez2006",
-        repo: "depend",
-        headers: {
-            "X-GitHub-Api-Version": "2022-11-28"
+const path = __nccwpck_require__(1017)
+const { createWriteStream, chmodSync } = __nccwpck_require__(7147);
+const { error } = __nccwpck_require__(6206);
+
+async function download_installer(path) {
+    try {
+        action.info("Downloading installer...");
+
+        const release = await request("GET /repos/{owner}/{repo}/releases/latest", {
+            owner: "arsenez2006",
+            repo: "depend",
+            headers: {
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+        });
+        if (release.status != 200) {
+            throw "Failed to get latest release";
         }
-    });
+
+        const assets = await request("GET /repos/{owner}/{repo}/releases/{release_id}/assets", {
+            owner: "arsenez2006",
+            repo: "depend",
+            release_id: release.data.id,
+            headers: {
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+        });
+        if (assets.status != 200) {
+            throw "Failed to get release assets list";
+        }
+
+        const asset_depend = assets.data.find(el => el.name == "depend");
+        if (asset_depend == null) {
+            throw "Failed to get asset \"depend\"";
+        }
+
+        const download_url = asset_depend.browser_download_url;
+        const response = await fetch(download_url);
+        if (!response.ok) {
+            throw "Cannot download installer";
+        }
+        const file = createWriteStream(path);
+        const file_stream = new WritableStream({
+            write(chunk) {
+                file.write(chunk);
+            }
+        });
+        await response.body.pipeTo(file_stream);
+
+        chmodSync(file.path, "755");
+
+        action.info(`Installer downloaded to ${file.path}`);
+        return true;
+    } catch (error) {
+        action.error(error);
+        return false;
+    }
 }
 
 async function run() {
-    const working_directory = path.join(process.cwd(), "deps");
-    await io.mkdirP(working_directory);
+    const working_directory = __nccwpck_require__.ab + "deps";
+    await io.mkdirP(__nccwpck_require__.ab + "deps");
+    const installer = __nccwpck_require__.ab + "depend";
 
-    await download_installer();
+    if (!await download_installer(__nccwpck_require__.ab + "depend")) {
+        action.setFailed("Failed to download installer");
+    }
 }
 
 run();
--- index.js
+++ index.js
@@ -1,26 +1,75 @@
-const path = require("path");
 const action = require("@actions/core");
 const cache = require("@actions/cache");
-const io = require("@actions/io")
+const io = require("@actions/io");
 const { request } = require("@octokit/request");
 
-async function download_installer() {
-    const release = await request("GET /repos/{owner}/{repo}/releases/latest", {
-        owner: "arsenez2006",
-        repo: "depend",
-        headers: {
-            "X-GitHub-Api-Version": "2022-11-28"
-        }
-    });
+const path = require("path")
+const { createWriteStream, chmodSync } = require("fs");
+const { error } = require("console");
 
-    console.log(release);
+async function download_installer(path) {
+    try {
+        action.info("Downloading installer...");
+
+        const release = await request("GET /repos/{owner}/{repo}/releases/latest", {
+            owner: "arsenez2006",
+            repo: "depend",
+            headers: {
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+        });
+        if (release.status != 200) {
+            throw "Failed to get latest release";
+        }
+
+        const assets = await request("GET /repos/{owner}/{repo}/releases/{release_id}/assets", {
+            owner: "arsenez2006",
+            repo: "depend",
+            release_id: release.data.id,
+            headers: {
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+        });
+        if (assets.status != 200) {
+            throw "Failed to get release assets list";
+        }
+
+        const asset_depend = assets.data.find(el => el.name == "depend");
+        if (asset_depend == null) {
+            throw "Failed to get asset \"depend\"";
+        }
+
+        const download_url = asset_depend.browser_download_url;
+        const response = await fetch(download_url);
+        if (!response.ok) {
+            throw "Cannot download installer";
+        }
+        const file = createWriteStream(path);
+        const file_stream = new WritableStream({
+            write(chunk) {
+                file.write(chunk);
+            }
+        });
+        await response.body.pipeTo(file_stream);
+
+        chmodSync(file.path, "755");
+
+        action.info(`Installer downloaded to ${file.path}`);
+        return true;
+    } catch (error) {
+        action.error(error);
+        return false;
+    }
 }
 
 async function run() {
     const working_directory = path.join(process.cwd(), "deps");
     await io.mkdirP(working_directory);
+    const installer = path.join(working_directory, "depend");
 
-    await download_installer();
+    if (!await download_installer(installer)) {
+        action.setFailed("Failed to download installer");
+    }
 }
 
 run();
